3ff2e56ddd776ddf9d9ab66e77c6a3f93c87f010
MahwishIzhar <izharmahwish@gmail.com>
2020-01-08 21:41:01 -0800

    Updates

---
+++ precache-manifest.3bd4b481e762f8b697076ed9bbc39010.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6462b484fe046fd7dfd4ce4f17c33571",
+    "url": "/Landing-Page/index.html"
+  },
+  {
+    "revision": "e5ee8ed0c8f8da7420b3",
+    "url": "/Landing-Page/static/css/main.85765f03.chunk.css"
+  },
+  {
+    "revision": "831e47711062e2ab87b7",
+    "url": "/Landing-Page/static/js/2.b9de73f0.chunk.js"
+  },
+  {
+    "revision": "378d5863afd68d4f461353cf03caf725",
+    "url": "/Landing-Page/static/js/2.b9de73f0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e5ee8ed0c8f8da7420b3",
+    "url": "/Landing-Page/static/js/main.87239d0e.chunk.js"
+  },
+  {
+    "revision": "ff9b279586c670fca686",
+    "url": "/Landing-Page/static/js/runtime-main.40e0f74f.js"
+  },
+  {
+    "revision": "8d714a09034b7ab8d6667acf5b8a6bc2",
+    "url": "/Landing-Page/static/media/background.8d714a09.PNG"
+  },
+  {
+    "revision": "85e2808ae2a1d0e4fbecfb0ed7f02d44",
+    "url": "/Landing-Page/static/media/cta-illustration.85e2808a.svg"
+  },
+  {
+    "revision": "851f0815fcf8d9ba3939a342052b5af3",
+    "url": "/Landing-Page/static/media/feature-icon-01.851f0815.svg"
+  },
+  {
+    "revision": "7e409417be562941e818e39d69d3566d",
+    "url": "/Landing-Page/static/media/feature-icon-02.7e409417.svg"
+  },
+  {
+    "revision": "6da0ea13d480e1b7b3b895379dce7090",
+    "url": "/Landing-Page/static/media/feature-icon-03.6da0ea13.svg"
+  },
+  {
+    "revision": "7eb920ca0b4ea593a1129dd3ed372b8c",
+    "url": "/Landing-Page/static/media/hero-back-illustration.7eb920ca.svg"
+  },
+  {
+    "revision": "aa0f14cb87e54d157f8fa6dd8b3308f4",
+    "url": "/Landing-Page/static/media/hero-top-illustration.aa0f14cb.svg"
+  },
+  {
+    "revision": "f52986709175b500cecd9dc13856bd7c",
+    "url": "/Landing-Page/static/media/logo.f5298670.svg"
+  },
+  {
+    "revision": "f5bcc1ccea322991b4b9b43b3f3ecefc",
+    "url": "/Landing-Page/static/media/pricing-illustration.f5bcc1cc.svg"
+  }
+]);
